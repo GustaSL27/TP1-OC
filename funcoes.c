42fac6a812eb70e1631c8efa1cc6904fed2537e2
Gustavo <gustavosl.2007@gmail.com>
2026-04-07 18:14:17 -0300

    a

--- funcoes.c
+++ funcoes.c
@@ -8,30 +8,30 @@ int funct7(char operacao[]){
         return 0x20; // 0100000
     }
     
-    if (strcmp(operacao, "and") == 0 ||
-        strcmp(operacao, "srl") == 0 ||
-        strcmp(operacao, "add") == 0 ||
-        strcmp(operacao, "or") == 0 ||
-        strcmp(operacao, "xor") == 0 ||
-        strcmp(operacao, "sll") == 0){
+    if ((strcmp(operacao, "and") == 0) ||
+        (strcmp(operacao, "srl") == 0) ||
+        (strcmp(operacao, "add") == 0) ||
+        (strcmp(operacao, "or") == 0) ||
+        (strcmp(operacao, "xor") == 0) ||
+        (strcmp(operacao, "sll") == 0)){
         return 0x00; // 0000000
     }
 
     return -1;
 }
 
-int funct3(char operacao[]) {
-    if (strcmp(operacao, "sub") == 0 |
-        strcmp(operacao, "lb") == 0 ||
-        strcmp(operacao, "sb") == 0 ||
-        strcmp(operacao, "beq") == 0 ||
-        strcmp(operacao, "add") == 0 ||
-        strcmp(operacao, "addi") == 0){
+int funct3(char operacao[]){
+    if ((strcmp(operacao, "sub") == 0) ||
+        (strcmp(operacao, "lb") == 0) ||
+        (strcmp(operacao, "sb") == 0) ||
+        (strcmp(operacao, "beq") == 0) ||
+        (strcmp(operacao, "add") == 0) ||
+        (strcmp(operacao, "addi") == 0)){
         return 0x0; // 000
     }
 
-    if (strcmp(operacao, "and") == 0 ||
-        strcmp(operacao, "andi") == 0){
+    if ((strcmp(operacao, "and") == 0) ||
+        (strcmp(operacao, "andi") == 0)){
         return 0x7; // 111
     }
 
@@ -39,20 +39,20 @@ int funct3(char operacao[]) {
         return 0x5; // 101
     }
 
-    if (strcmp(operacao, "ori") == 0 ||
-        strcmp(operacao, "or") == 0){
+    if ((strcmp(operacao, "ori") == 0) ||
+        (strcmp(operacao, "or") == 0)){
         return 0x6; // 110
     }  
 
-    if (strcmp(operacao, "lh") == 0 ||
-        strcmp(operacao, "sh") == 0 ||
-        strcmp(operacao, "bne") == 0||
-        strcmp(operacao, "sll") == 0){
+    if ((strcmp(operacao, "lh") == 0) ||
+        (strcmp(operacao, "sh") == 0) ||
+        (strcmp(operacao, "bne") == 0) ||
+        (strcmp(operacao, "sll") == 0)){
         return 0x1; // 001
     }
 
-    if (strcmp(operacao, "lw") == 0 ||
-        strcmp(operacao, "sw") == 0){
+    if ((strcmp(operacao, "lw") == 0) ||
+        (strcmp(operacao, "sw") == 0)){
         return 0x2; // 010
     }
 
@@ -63,39 +63,39 @@ int funct3(char operacao[]) {
     return 0;
 }
 
-int opcode(char operacao[]) {
-    if (strcmp(operacao, "sub") == 0 || 
-        strcmp(operacao, "and") == 0 ||
-        strcmp(operacao, "srl") == 0 ||
-        strcmp(operacao, "add") == 0 ||
-        strcmp(operacao, "or") == 0 ||
-        strcmp(operacao, "xor") == 0 ||
-        strcmp(operacao, "sll") == 0){
+int opcode(char operacao[]){
+    if ((strcmp(operacao, "sub") == 0) || 
+        (strcmp(operacao, "and") == 0) ||
+        (strcmp(operacao, "srl") == 0) ||
+        (strcmp(operacao, "add") == 0) ||
+        (strcmp(operacao, "or") == 0) ||
+        (strcmp(operacao, "xor") == 0) ||
+        (strcmp(operacao, "sll") == 0)){
         return 0x33; // 0110011
     }
-    if (strcmp(operacao, "ori") == 0 ||
-        strcmp(operacao, "addi") == 0 ||
-        strcmp(operacao, "andi") == 0){
+    if ((strcmp(operacao, "ori") == 0) ||
+        (strcmp(operacao, "addi") == 0) ||
+        (strcmp(operacao, "andi") == 0)){
         return 0x13; // 0010011
     }
-    if (strcmp(operacao, "lb") == 0 ||
-        strcmp(operacao, "lh") == 0 ||
-        strcmp(operacao, "lw") == 0){
+    if ((strcmp(operacao, "lb") == 0) ||
+        (strcmp(operacao, "lh") == 0) ||
+        (strcmp(operacao, "lw") == 0)){
         return 0x03; // 0000011
     }
-    if (strcmp(operacao, "sb") == 0||
-        strcmp(operacao, "sh") == 0||
-        strcmp(operacao, "sw") == 0){
+    if ((strcmp(operacao, "sb") == 0) ||
+        (strcmp(operacao, "sh") == 0) ||
+        (strcmp(operacao, "sw") == 0)){
         return 0x23; // 0100011
     }
-    if (strcmp(operacao, "beq") == 0||
-        strcmp(operacao, "bne") == 0){
+    if ((strcmp(operacao, "beq") == 0) ||
+        (strcmp(operacao, "bne") == 0)){
         return 0x63; // 1100011
     }
     return 0;
 }
 
-int NumeroRegistrador(char termo[]) {
+int NumeroRegistrador(char termo[]){
     if (termo[0] == 'x') {
         return atoi(&termo[1]); //converte string pra inteiro
     }
@@ -117,7 +117,7 @@ int ImediatoMemoria(char termo[]){
     return atoi(termo);
 }
 
-int RegMemoria(char termo[]) {
+int RegMemoria(char termo[]){
     char *ptr = strchr(termo, 'x');  // cria um ponteiro apontando para o "x"
     if (ptr != NULL) {
         return atoi(ptr + 1);
